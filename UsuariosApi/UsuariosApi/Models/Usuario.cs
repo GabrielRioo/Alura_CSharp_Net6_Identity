afcf3ae3206200220b38cd29a306e6c13ed3eb32
GabrielRioo <gabrielpaivario@gmail.com>
2023-10-27 20:53:42 -0300

    feat(:sparkles:): create the UsuarioDbContext, the IdentityUser and dependencies

--- UsuariosApi/UsuariosApi/Models/Usuario.cs
+++ UsuariosApi/UsuariosApi/Models/Usuario.cs
@@ -1,5 +1,14 @@
-﻿namespace UsuariosApi.Models;
+﻿using Microsoft.AspNetCore.Identity;
 
-public class Usuario
+namespace UsuariosApi.Models;
+
+public class Usuario: IdentityUser
 {
+	// Herda todas as propriedades do IdentityUser
+	public DateTime DataNascimento { get; set; }
+
+	public Usuario() : base()
+	{
+
+	}
 }
